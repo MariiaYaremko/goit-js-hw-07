4a11ad07a14005618664a586b34726d43726b573
MariiaYaremko <83749699+MariiaYaremko@users.noreply.github.com>
2021-09-24 12:07:04 +0300

    hw-07
    виправлено розмір шрифту і сhange на input

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const input = document.querySelector('input#font-size-control');
 console.log(input);
 const textRef = document.querySelector('span#text');
 console.log(textRef);
-input.addEventListener('change', event => {
+input.addEventListener('input', event => {
     textRef.style.fontSize = event.target.value + 'px';
    }
 )
